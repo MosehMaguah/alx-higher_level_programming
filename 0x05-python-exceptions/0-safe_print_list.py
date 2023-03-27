ee8a12733654cffdaba109320731fc639f3844c6
MosehMaguah <Mosehmaguah@gmail.com>
2023-03-27 18:01:29 +0300

    ssafe print

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    try:
-        for i in range(x):
-            print(my_list[i], end=' ')
-    except IndexError:
-        pass
+    count = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except IndexError:
+            pass
     print()
-    return min(x, len(my_list))
-
+    return count
