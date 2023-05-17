78c0de10d79bf1e1fe7492c7dc431d02b7ad34f9
MosehMaguah <Mosehmaguah@gmail.com>
2023-05-17 20:02:49 +0300

    states

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,7 +1,12 @@
--- creates the database hbtn_0d_usa and the table states
--- create database hbtn_0d_usa
+-- Create the hbtn_0d_usa database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
---use hbtn_0d_usa
+
+-- Switch to the hbtn_0d_usa database
 USE hbtn_0d_usa;
--- create table states
-CREATE TABLE IF NOT EXISTS states (id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(256) NOT NULL));
+
+-- Create the states table
+CREATE TABLE IF NOT EXISTS states (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(256) NOT NULL
+);
+
