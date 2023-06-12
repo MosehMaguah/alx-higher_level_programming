3ac8ef8f92132ce079a5a93b3403d716199b2cb3
MosehMaguah <Mosehmaguah@gmail.com>
2023-06-12 22:04:50 +0300

    Javascript 0x12 tasks updates

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// Script that prints strings using loop
+
+let i;
+const myStrings = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (i = 0; i < 3; i++) {
+  console.log(myStrings[i]);
+}
