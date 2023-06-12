999c17fceeb38e68190f00d71b45ec05e249a4b7
MosehMaguah <Mosehmaguah@gmail.com>
2023-06-13 01:21:00 +0300

    Loop to languages

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,7 +2,7 @@
 // Script that prints strings using loop
 
 let i;
-const myStrings = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const myStrings = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (i = 0; i < 3; i++) {
   console.log(myStrings[i]);
 }
