729c257fc807ac108e4acfbdee5f163e38339877
MosehMaguah <Mosehmaguah@gmail.com>
2023-06-18 02:36:28 +0300

    Cities in state

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,25 +1,29 @@
 #!/usr/bin/python3
 
 """
-  Script that changes the name of a state object from the database
-  hbtn_0e_6_usa.
+  contains a base and city class.
 """
 
-import sys
-from model_state import Base, State
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import ForeignKey
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy import Integer
 
 
-if __name__ == '__main__':
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-    Session = sessionmaker(bind=engine)
-    session = Session()
+Base = declarative_base()
 
-    stateUpdated = session.query(State).filter(State.id == 2).first()
 
-    if stateUpdated:
-        stateUpdated.name = 'New Mexico'
-        session.commit()
+class City(Base):
+    """
+        City class inherits the Base class
+        Attributes:
+            id (int)
+            name (string)
+            state_id (string)
+    """
+    __tablename__ = 'cities'
+
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
