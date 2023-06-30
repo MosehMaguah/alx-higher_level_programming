8527ea404cd119db7733146dc4544de59e2075e4
MosehMaguah <Mosehmaguah@gmail.com>
2023-06-30 20:59:39 +0300

    catch me

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message
-curl -sX PUT -L -H "Origin: HolbertonSchool" -d "user_id=98"
-0.0.0.0:5000/catch_me
+curl -sX PUT -L -d "user_id=98" -H "Origin:HolbertonSchool" "0.0.0.0:5000/catch_me"
